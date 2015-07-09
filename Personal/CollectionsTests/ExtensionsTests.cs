cb66cc826199991c2bf86d0f0332bff6cf3feb20
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-07-09 14:08:49 -0700

    Add the test project.

---
+++ Personal/CollectionsTests/ExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CollectionsTests
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            throw new Exception();
+        }
+    }
+}
