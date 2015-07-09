16e25e76b6d1f1a28a1323eaa07818e8d0cca0f5
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-07-09 15:04:36 -0700

    Adding tests for many permutations.

--- Personal/CollectionsTests/ExtensionsTests.cs
+++ Personal/CollectionsTests/ExtensionsTests.cs
@@ -1,14 +1,66 @@
 ﻿using System;
 using Xunit;
+using Collections;
 
 namespace CollectionsTests
 {
     public class ExtensionsTests
     {
         [Fact]
-        public void TestMethod1()
+        public void TestNaturalSort()
         {
-            throw new Exception();
+            Comparison<int> compare = (a, b) => a - b;
+            const int count = 9;
+            for (var i = 0; i < count; i++)
+            {
+                var original = new int[i];
+                var copy1 = new int[i];
+                var copy2 = new int[i];
+
+                do
+                {
+                    Array.Copy(original, copy1, original.Length);
+                    Array.Copy(original, copy2, original.Length);
+
+                    copy1.NaturalMergeSort(compare);
+                    Array.Sort(copy2, compare);
+
+                    AssertEquals(copy1, copy2);
+                }
+                while (Increment(original, i - 1));
+            }
+        }
+
+        private void AssertEquals(int[] copy1, int[] copy2)
+        {
+            for (var i = 0; i < copy1.Length; i++)
+            {
+                if (copy1[i] != copy2[i])
+                {
+                    Assert.True(false);
+                }
+            }
+        }
+
+        private bool Increment(int[] original, int count)
+        {
+            for (var i = 0; i < original.Length; i++)
+            {
+                if (original[i] == count)
+                {
+                    if (i == original.Length - 1)
+                    {
+                        return false;
+                    }
+                    original[i] = 0;
+                    continue;
+                }
+
+                original[i]++;
+                return true;
+            }
+
+            return false;
         }
     }
 }
