e1ee52ff803b2a5e5e91d367b9822586ca802f52
Cyrus Najmabadi <cyrusn@microsoft.com>
2015-08-05 15:10:14 -0700

    Brownian tree code.

---
+++ Personal/BrownianTree/BrownianTree.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    internal class BrownianTree
+    {
+        public bool[,] Generate(int width, int height,
+            Action<int, int> callback = null)
+        {
+            var values = new bool[width, height];
+
+            var halfWidth = width / 2;
+            var halfHeight = height / 2;
+
+            var outerRadius = Math.Min(halfWidth, halfHeight) * 0.95;
+            var outerRadiusSquared = (int)outerRadius * outerRadius;
+
+            var random = new Random();
+            while (true)
+            {
+                var x = halfWidth;
+                var y = halfHeight;
+
+                while (true)
+                {
+                    var newX = x + NextDirection(random);
+                    var newY = y + NextDirection(random);
+
+                    if (newX < 0 || newX >= width ||
+                        newY < 0 || newY >= height)
+                    {
+                        throw new Exception();
+                        // Walked off the side.  Place another random point.
+                        // continue;
+                    }
+
+                    if (newX == x && newY == y)
+                    {
+                        continue;
+                    }
+
+                    // Hit an existing point.
+                    var hitSomething = values[newX, newY];
+
+                    if (!hitSomething)
+                    {
+                        var deltaX = newX - halfWidth;
+                        var deltaY = newY - halfHeight;
+                        var sumSquared = deltaX * deltaX + deltaY * deltaY;
+                        if (sumSquared >= outerRadiusSquared)
+                        {
+                            hitSomething = true;
+                        }
+                    }
+                    if (hitSomething)
+                    {
+                        //if (!allowAdjacent && HasMultipleAdjacent(values, x, y))
+                        //{
+                        //    break;
+                        //}
+
+                        values[x, y] = true;
+                        if (callback != null)
+                        {
+                            callback(x, y);
+                        }
+
+                        // If we're at the middle, we're done.  Otherwise,
+                        // keep going.
+                        if (x == halfWidth && y == halfHeight)
+                        {
+                            return values;
+                        }
+
+                        break;
+                    }
+
+                    // Keep walking
+                    x = newX;
+                    y = newY;
+                }
+            }
+        }
+
+        private static int NextDirection(Random random)
+        {
+            return random.Next(3) - 1;
+        }
+    }
+}
---
+++ Personal/BrownianTree/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+#if false
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var form = new Form1();
+            Application.Run(form);
+#else
+            int width = 1000, height = 1000;
+
+            var bitmap = new Bitmap(width, height);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.FillRectangle(Brushes.Black, 0, 0, width, height);
+            int halfWidth = width / 2, halfHeight = height / 2;
+            // var radiusSquared = halfWidth * halfWidth + halfHeight * halfHeight;
+            var bestRadiusSquared = int.MaxValue;
+
+            var values = new BrownianTree().Generate(width, height, (x, y) =>
+            {
+                var deltaX = halfWidth - x;
+                var deltaY = halfHeight - y;
+                var radiusSquared = deltaX * deltaX + deltaY * deltaY;
+                if (radiusSquared < bestRadiusSquared)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Radius: " + Math.Sqrt(radiusSquared));
+                    bestRadiusSquared = radiusSquared;
+                }
+
+                Console.Write(".");
+            });
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var  j = 0; j < height; j++)
+                {
+                    if (values[i, j])
+                    {
+                        graphics.FillRectangle(Brushes.White, i, j, 1, 1);
+                    }
+                }
+            }
+
+            bitmap.Save(@"c:\temp\testing1.bmp");
+#endif
+        }
+    }
+}
